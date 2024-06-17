14ba94d7b0ff6c80c8505034462f732dfd75741b
Archie Malvern <123561414+archiemal@users.noreply.github.com>
2024-06-17 15:12:35 +0100

    Update WeatherFetch.cs

--- WeatherWatch/WeatherFetch.cs
+++ WeatherWatch/WeatherFetch.cs
@@ -12,7 +12,7 @@ namespace WeatherWatch
         {
             using (HttpClient client = new HttpClient())
             {
-                string apiKey = "d2fef12af28e09e161b8745d963664e5";
+                string apiKey = "[ENTER API KEY HERE]";
                 string units = "metric";
                 string url = $"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={apiKey}&units={units}"; // This is the URL that pulls data from OpenWeatherMap's API
                 HttpResponseMessage response = await client.GetAsync(url);
